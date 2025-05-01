dbc117d06b11397113ee612815a342127755ea89
somu <esomasekhar008@gmail.com>
2025-05-01 02:15:23 -0400

    Pushing code

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -138,3 +138,13 @@ class TestAccountService(TestCase):
         }
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
+    def test_get_account(self):
+        """It should Read a single Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.get(
+            f"{BASE_URL}/{account.id}", content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data["name"], account.name)
+    
